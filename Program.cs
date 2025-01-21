5650d370155702677768b2500610bb9c0bb2e24d
Diogo Conceição dos Santos <20231170150047@ifrn.local>
2025-01-21 17:47:29 -0300

    update and feat: updating and adding all files

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Personagem p1 = new Personagem("Jack");
+        Personagem jogador = new Personagem("Jack");
+
+        jogador.PegarItem("Cajado mágico");
+        jogador.PegarItem("poção de cura");
+        jogador.MostrarInventario();
+
+        jogador.Atacar("Bruxo");
+        jogador.LargarItem("Cajado mágico");
+        jogador.MostrarInventario();
+
+
     }
 }
